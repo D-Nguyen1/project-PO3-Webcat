0860272dbbcda63417f632f01b30c3fd21147fce
Mo-070 <143715500+Mo-070@users.noreply.github.com>
2024-03-08 14:26:54 +0100

    Bezig

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import fs from "node:fs"
 const app = express();
 const port = 3000;
 app.use(cors());
@@ -7,5 +8,26 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`example app listening on port ${port}`);
+});
+ 
+app.get("/data/db", (req , res) =>{
+    res.setHeader("content-type" , "application/json;charset=UTF-8");
+    fs.readFile("data/db.json" , function(err, data){
+        res.send(data);
+    });
+});
+
+app.get("/data/naruto", (req , res) =>{
+    res.setHeader("content-type" , "application/json;charset=UTF-8");
+    fs.readFile("data/naruto.json" , function(err, data){
+        res.send(data);
+    });
+});
+
+app.get("/data/onepiece", (req , res) =>{
+    res.setHeader("content-type" , "application/json;charset=UTF-8");
+    fs.readFile("data/onepiece.json" , function(err, data){
+        res.send(data);
+    });
 });
