8e5a1bf57e378964d42272067a636f1cb970d0b9
Mo-070 <143715500+Mo-070@users.noreply.github.com>
2024-03-16 17:17:58 +0100

    Bezig

--- js/anime-op.js
+++ js/anime-op.js
@@ -1,2 +1,35 @@
 console.log("test loading...");
-const animeContainer= document.querySelector(".cart-container");
+const animeContainer= document.querySelector(".anime-container");
+
+fetch("http://localhost:3000/data/anime-series")
+  .then((data) => data.json())
+  .then((myjsondata) => showCards(myjsondata));
+ 
+function showCards(animeSeries) {
+  console.log(animeSeries);
+  let htmlCode = "";
+  for (let i = 0; i < animeSeries.length; i++) {
+    const animeSerie = animeSeries[i];
+    htmlCode += createCard(animeSerie);
+  }
+  cardContainer.innerHTML = htmlCode;
+}
+ 
+function createCard(animeSerie) {
+  const card = `
+    <div class="card">
+      <img src="${animeSerie.imageURL}">
+      <div class="card-content">
+        <h1 class="card-title">${animeSerie.name}</h1>
+        <p class="card-description">${animeSerie.description}</p>
+        <p class="card-details">${animeSerie.creator}<br>
+        ${animeSerie.character}<br>
+        ${animeSerie.date}<br>
+        ${animeSerie.category}<br>
+        </p>
+      </div>
+    </div>
+ `;
+  return card;
+}
+ 
